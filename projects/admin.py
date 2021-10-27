269d47f46b76a3d8a4e878635bff07c200720a1d
Nyagahisaac <isaac.ireri@student.moringaschool.com>
2021-10-27 08:39:35 +0300

    create my api's in my serializers.py file

--- projects/admin.py
+++ projects/admin.py
@@ -3,22 +3,6 @@ from .models import Rates,Project_Post,Profile,Reviews
 
 # Register your models here.
 admin.site.register(Project_Post)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 admin.site.register(Profile)
 admin.site.register(Rates)
 admin.site.register(Reviews)
